ff652c7e49983648608f320753e8cad492837e2f
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-11-13 19:51:03 +0000

    Adds 'remove_rows_with_missing_ratings' function

--- tabular_data.py
+++ tabular_data.py
@@ -1,4 +1,17 @@
+import numpy as np
 import pandas as pd
 
 print("Start Reading!")
-pd.read_csv("https://aicore-project-files.s3.eu-west-1.amazonaws.com/airbnb-property-listings.zip")
+raw_data = pd.read_csv("listing.csv")
+# print(raw_data)
+# print(raw_data.info())
+# print(raw_data.describe())
+# print(raw_data["Value_rating"])
+# print(raw_data[raw_data["Value_rating"].notna()])
+
+def remove_rows_with_missing_ratings(raw_data):
+    raw_data.dropna(subset=["Value_rating"], inplace=True)
+    return raw_data
+
+raw_data_with_ratings = remove_rows_with_missing_ratings(raw_data)
+print(raw_data_with_ratings)
