1d7fcbd5af2d12b8c13060ef3cff90fc223685dd
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-11-29 21:48:06 +0000

    Adds method to download resized images

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import glob
+import os
 
 # def test():
 #     img_path = "/home/timothy/Documents/Aicore/Airbnb-Property-Modelling/images/0a26e526-1adf-4a2a-888d-a05f7f0a2f33/0a26e526-1adf-4a2a-888d-a05f7f0a2f33-a.png"
@@ -12,45 +13,41 @@ import glob
 class PrepareImages:
 
     def __init__(self):
-        self.image_dict = {}
+        self.image_name_list = []
+        self.image_list = []
         self.height_list = []
-        self.minimum_height = 0
-        self.resized_image_dict = []
+        self.resized_image_list = []
 
     def __import_images(self):
         for filename in glob.glob("/home/timothy/Documents/Aicore/Airbnb-Property-Modelling/images/*/*.png"):
             print(filename)
-            img_name = filename[-42:]
-            print(img_name)
             img = Image.open(filename)
             if img.mode == "RGB":
-                self.image_dict[img_name] = img
+                img_name = filename[-42:]
+                self.image_name_list.append(img_name)
+                self.image_list.append(img)
                 self.height_list.append(img.height)
-        print(f"There are {len(self.image_dict)} RGB images")
-
-    def __find_minimum_image_height(self):
-        for img in self.image_dict.values():
-            self.height_list.append(img.height)
-        self.minimum_height = min(self.height_list)
-        print(f"Minimum height: {self.minimum_height}")
+        print(f"There are {len(self.image_list)} RGB images")
 
     def __resize_images(self):
-        for img in self.image_dict.values():
+        minimum_height = min(self.height_list)
+        print(f"The minimum height is {minimum_height}")
+        for img in self.image_list:
             aspect_ratio = img.width / img.height
-            new_width = int(self.minimum_height * aspect_ratio)
-            new_size = (new_width, self.minimum_height)
+            new_width = int(minimum_height * aspect_ratio)
+            new_size = (new_width, minimum_height)
             resized_image = img.resize(new_size)
-            self.resized_image_dict.append(resized_image)
+            self.resized_image_list.append(resized_image)
 
-    def __install_resized_images(self):
-        for img in self.resized_image_dict:
-            pass
-            
+    def __download_resized_images(self):
+        for img_name, img in zip(self.image_name_list, self.resized_image_list):
+            file_path = os.path.join("/home/timothy/Documents/Aicore/Airbnb-Property-Modelling/processed_images", img_name)
+            img.save(file_path)
+
     def do_whole_resize(self):
         PrepareImages.__import_images(self)
-        PrepareImages.__find_minimum_image_height(self)
         PrepareImages.__resize_images(self)
-        # PrepareImages.__install_resized_images(self)
+        PrepareImages.__download_resized_images(self)
 
 if __name__ == '__main__':
     resize = PrepareImages()
