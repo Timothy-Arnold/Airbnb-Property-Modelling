a068c369ee68d691d287e4828055096e4d40b8d0
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-11-30 23:55:45 +0000

    Adds method to filter descriptions

--- tabular_data.py
+++ tabular_data.py
@@ -4,6 +4,7 @@ from ast import literal_eval
 
 def remove_rows_with_missing_ratings(raw_data):
     raw_data.dropna(subset=["Value_rating"], inplace=True)
+    raw_data.drop(raw_data.columns[-1], axis = 1, inplace=True)
     return raw_data
 
 def combine_description_strings(desc):
@@ -12,10 +13,19 @@ def combine_description_strings(desc):
     full_desc = ' '.join(desc_list_without_empty_quotes)
     return full_desc
 
+def is_valid_description(description):
+    try: 
+        description_type = type(literal_eval(description))
+        if description_type == list:
+            return description
+        else:
+            return np.nan
+    except:
+        return np.nan
+
 def fix_description_strings(df):
+    df['Description'] = df['Description'].apply(is_valid_description)
     df.dropna(subset=["Description"], inplace=True)
-    # Information for 586 was found to be completely in the wrong places
-    df.drop([586], inplace=True)
     df["Description"] = df["Description"].apply(combine_description_strings)
     print(df.info())
     return df
@@ -25,7 +35,7 @@ def set_default_feature_values(df):
     return df
 
 def clean_tabular_data(df):
-    raw_data_with_ratings = remove_rows_with_missing_ratings(raw_data)
+    raw_data_with_ratings = remove_rows_with_missing_ratings(df)
     raw_data_with_description = fix_description_strings(raw_data_with_ratings)
     raw_data_default_features = set_default_feature_values(raw_data_with_description)
     print(raw_data_default_features)
@@ -33,6 +43,6 @@ def clean_tabular_data(df):
     return raw_data_default_features
 
 if __name__ == '__main__':
-    raw_data = pd.read_csv("listing.csv")
+    raw_data = pd.read_csv("listing.csv", index_col=0)
     clean_data = clean_tabular_data(raw_data)
     clean_data.to_csv("clean_tabular_data.csv")
