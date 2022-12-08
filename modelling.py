21a9faf791e96174069be740e74ec8fd2925e787
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-12-08 23:40:30 +0000

    Adds custom_tune_regression_model_hyperparameters function

--- modelling.py
+++ modelling.py
@@ -1,3 +1,4 @@
+import itertools
 import numpy as np
 import pandas as pd
 
@@ -13,13 +14,14 @@ from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import ShuffleSplit
 from sklearn.model_selection import validation_curve
+from torchvision.models import resnet50, resnet18
 
 np.random.seed(2)
 
 #Split dataset
 clean_data = pd.read_csv("clean_tabular_data.csv")
 X, y = tabular_data.load_airbnb(clean_data, "Price_Night")
-print(X.describe())
+# print(X.describe())
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
@@ -27,29 +29,52 @@ X_test, X_validation, y_test, y_validation = train_test_split(
     X_test, y_test, test_size=0.5
 )
 
-#Fit to model
-sgd = SGDRegressor()
-sgd.fit(X_train, y_train)
-y_hat_train = sgd.predict(X_train)
-y_hat_test = sgd.predict(X_test)
+def create_first_model():
+    sgd = SGDRegressor() 
+    sgd.fit(X_train, y_train)
+    y_hat_train = sgd.predict(X_train)
+    y_hat_validation = sgd.predict(X_validation)
+    return [y_hat_train, y_hat_validation]
 
-def print_errors():
+def print_errors(y_hat_train, y_hat_validation):
     train_rmse = mean_squared_error(y_train, y_hat_train, squared=False)
-    test_rmse = mean_squared_error(y_test, y_hat_test, squared=False)
-    test_mae = mean_absolute_error(y_test, y_hat_test)
-    test_mse = mean_squared_error(y_test, y_hat_test)
+    validation_rmse = mean_squared_error(y_validation, y_hat_validation, squared=False)
+    validation_mae = mean_absolute_error(y_validation, y_hat_validation)
+    validation_mse = mean_squared_error(y_validation, y_hat_validation)
     train_r2 = r2_score(y_train, y_hat_train)
-    test_r2 = r2_score(y_test, y_hat_test)
+    validation_r2 = r2_score(y_validation, y_hat_validation)
 
     print("Mean root squared error on Training set: ", train_rmse)
-    print("Mean root squared error on Test set: ", test_rmse)
-    print("Mean absolute error on Test set: ", test_mae)
-    print("Mean squared error on Test set: ", test_mse)
+    print("Mean root squared error on Validation set: ", validation_rmse)
+    print("Mean absolute error on Validation set: ", validation_mae)
+    print("Mean squared error on Validation set: ", validation_mse)
     print("R squared on Train set: ", train_r2)
-    print("R squared on Test set: ", test_r2)
+    print("R squared on Validation set: ", validation_r2)
 
-def custom_tune_regression_model_hyperparameters():
-    pass
-
+def custom_tune_regression_model_hyperparameters(model_class, 
+    X_train, y_train, X_validation, y_validation, X_test, y_test, 
+    hyper_values_list_dict):
+    fitted_model_list = []
+    keys = hyper_values_list_dict.keys()
+    vals = hyper_values_list_dict.values()
+    for instance in itertools.product(*vals):
+        hyper_values_dict = dict(zip(keys, instance))
+        models_list = {"ResNet-50" : resnet50, "ResNet-18" : resnet18, "SGDRegression" : SGDRegressor}
+        model = models_list[model_class](**hyper_values_dict)
+        model.fit(X_train, y_train)
+        y_hat_validation = model.predict(X_validation)
+        validation_rmse = mean_squared_error(y_validation, y_hat_validation, squared=False)
+        validation_r2 = r2_score(y_validation, y_hat_validation)
+        performance_metrics_dict = {"validation_RMSE": validation_rmse, "validation_R2": validation_r2}
+        model_details = [model, hyper_values_dict, performance_metrics_dict]
+        fitted_model_list.append(model_details)
+    best_model_list = min(fitted_model_list, key=lambda x: x[2]["validation_RMSE"])
+    print(f"Best model by validation_RMSE metric:\n{best_model_list}")
+    return best_model_list
+        
 if __name__ == '__main__':
-    print_errors()
+    best_model_list = custom_tune_regression_model_hyperparameters("SGDRegression", 
+    X_train, y_train, X_validation, y_validation, X_test, y_test, 
+    hyper_values_list_dict={"penalty": ["l1", "l2", "elasticnet"],
+    "early_stopping": [True, False], 
+    "learning_rate": ["constant", "invscaling", "adaptive"]})
