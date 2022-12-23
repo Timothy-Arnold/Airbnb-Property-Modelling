960671ea489aaa4a7cd61e4cd3082519405d8129
Timothy Arnold <timothy.cy.arnold@gmail.com>
2022-12-23 21:27:44 +0000

    Adds first part of train function

--- neural_network.py
+++ neural_network.py
@@ -12,8 +12,12 @@ transform = transforms.PILToTensor()
 
 clean_data = pd.read_csv("clean_tabular_data.csv")
 X, y = tabular_data.load_airbnb(clean_data, "Price_Night")
+print(type(X))
 features = X.to_numpy()
+print(type(features))
 label = y.to_numpy()
+number_of_features = len(features[1])
+print(f"Number of features: {number_of_features}")
 
 np.random.seed(2)
 
@@ -33,14 +37,13 @@ dataset = AirbnbNightlyPriceImageDataset()
 # print(dataset[3])
 # print(type(dataset))
 # print(type(dataset[3][0]))
-# print(len(dataset))
 
 train_set, test_set = torch.utils.data.random_split(dataset, [int(len(dataset) * 17/20), len(dataset) - int(len(dataset) * 17/20)])
 train_set, validation_set = torch.utils.data.random_split(train_set, [int(len(train_set) * 14/17), len(train_set) - int(len(train_set) * 14/17)])
 
-print(len(train_set))
-print(len(validation_set))
-print(len(test_set))
+print("Size of train set: " + str(len(train_set)))
+print("Size of validation set: " + str(len(validation_set)))
+print("Size of test set: " + str(len(test_set)))
 
 batch_size = 4
 
@@ -48,8 +51,30 @@ train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
 validation_loader = DataLoader(validation_set, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
-# print(type(train_set))
+print(type(train_set))
 # print(type(train_loader))
 
 example = next(iter(train_loader))
-print(example)
+# print(example)
+features, label = example
+features = features.reshape(batch_size, -1)
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # Initialize parameters
+        self.linear_layer = torch.nn.Linear(number_of_features, 1)
+
+    def forward(self, features):
+        # Use the layers to process the features
+        features = features.type(torch.float)
+        return self.linear_layer(features)
+
+def train(model, data_loader, epochs):
+    for epoch in range(epochs):
+        example = next(iter(data_loader))
+        features, label = example
+        features = features.reshape(batch_size, -1)
+        print(model(features))
+
+train(NeuralNetwork(), train_loader, 1)
